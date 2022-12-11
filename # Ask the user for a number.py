1fce64547611b486bf25cd51768f5d78cc027374
subhash kumar yadav <110653851+Subhashkunaryadav@users.noreply.github.com>
2022-12-11 14:50:43 +0530

    Add files via upload

---
+++ # Ask the user for a number.py
@@ -0,0 +1,10 @@
+# Ask the user for a number
+number = int(input("Enter a number: "))
+
+# Check if the number is positive or negative
+if number >= 0:
+    # If the number is positive, print a message
+    print(f"{number} is positive.")
+else:
+    # If the number is negative, print a different message
+    print(f"{number} is negative.")
